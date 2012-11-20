bebe966e8e18bb5928afb676db867667dea28310
Alex Poslavsky <alexander.poslavsky@gmail.com>
2012-11-20 21:14:12 +0100

    working simple

--- apos_hello.php
+++ apos_hello.php
@@ -41,7 +41,7 @@ class apos_hello {
 		add_action( 'init', array( $this, 'textdomain' ) );
 
 		// Register admin styles and scripts
-		// add_action( 'admin_print_styles', array( $this, 'register_admin_styles' ) );
+		add_action( 'admin_print_styles', array( $this, 'register_admin_styles' ) );
 		// add_action( 'admin_enqueue_scripts', array( $this, 'register_admin_scripts' ) );
 
 		// Register site styles and scripts
@@ -50,6 +50,8 @@ class apos_hello {
 
 		register_activation_hook( __FILE__, array( $this, 'activate' ) );
 		register_deactivation_hook( __FILE__, array( $this, 'deactivate' ) );
+
+		add_action('admin_init', array( $this, 'apos_bar_admin_init' ) );
 
 	    /*
 	     * TODO:
@@ -68,17 +70,87 @@ class apos_hello {
 
 	} // end constructor
 
-	public function apos_hello_menu() {
-		add_options_page( 'My Plugin Options', 'Apos Bar', 'manage_options', 'my-unique-identifier', array($this, 'apos_hello_options') );
+	public function apos_bar_admin_init($value='')
+	{
+		// register_setting( 'apos_bar_options', 'apos_bar_options', array( $this,'apos_bar_options_validate') );
+		register_setting( 'apos_bar_options', 'apos_bar_options' );
+
+		add_settings_section('plugin_main', 'Main Settings2', 'plugin_section_text', 'apos-bar');
+		add_settings_field('apos_bar_text_string', 'Message or Call to Action', array( $this,'plugin_setting_string'), 'apos-bar', 'plugin_main');
+		add_settings_field('apos_bar_text_radio', 'Header / Footer', array( $this,'apos_bar_setting_radio'), 'apos-bar', 'plugin_main');
+		add_settings_field('apos_bar_text_string2', 'Button Text', array( $this,'plugin_setting_string2'), 'apos-bar', 'plugin_main');
 	}
 
-	public function apos_hello_options() {
-		if ( !current_user_can( 'manage_options' ) )  {
-		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+	public function plugin_setting_string($id) {
+		$options = get_option('apos_bar_options');
+		echo "<input id='plugin_text_string' name='apos_bar_options[text]' size='120' type='text' value='{$options['text']}' />";
+	}
+
+	public function plugin_setting_string2($id) {
+		$options = get_option('apos_bar_options');
+		echo "<input id='plugin_text_string' name='apos_bar_options[button]' size='40' type='text' value='{$options['button']}' />";
+	}
+
+	public function apos_bar_setting_radio($id) {
+		$options = get_option('apos_bar_options');
+		if (! $options["apos_bar_radio"]) { $options["apos_bar_radio"]="header"; }
+		?>
+		<label>
+			<input name="apos_bar_options[load]" type="radio" value="header" <?php checked('one', $options['load']); ?> /> Header
+		</label><br />
+		<label>
+			<input name="apos_bar_options[load]" type="radio" value="footer" <?php checked('two', $options['load']); ?> /> Footer
+		</label><br />
+		<br>
+		<span style="color:#666666;">Load the Apos Bar code in the header or footer</span>
+		<?php
+	}
+
+	public function apos_bar_options_validate($input) {
+		$newinput['text_string'] = trim($input['text_string']);
+		if(!preg_match('/^[a-z0-9]{32}$/i', $newinput['text_string'])) {
+		$newinput['text_string'] = '';
 		}
-		echo '<div class="wrap">';
-		echo '<p>Here is where the form would go if I actually had options.</p>';
-		echo '</div>';
+		return $newinput;
+	}
+
+	public function apos_hello_menu() {
+		add_options_page( 'My Plugin Options', 'Apos Bar', 'manage_options', 'apos-bar', array($this, 'apos_bar_options_page') );
+	}
+
+	function apos_bar_options_page() {
+		if ( !current_user_can( 'manage_options' ) )  {
+			wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+		}
+		?>
+		<div class="wrap">
+		<h2>My custom plugin</h2>
+		Options relating to the Custom Plugin.
+		<form action="options.php" method="post">
+
+		<?php settings_fields('apos_bar_options'); ?>
+		<?php do_settings_sections('apos-bar'); ?>
+		 
+		<?php submit_button(); ?>
+
+		</form></div>
+		 
+		<h2>Preview</h2>
+		<?php $options = get_option('apos_bar_options'); ?>
+		<div class="apos_hello">
+		    <span style="font-family: 'Arial, Helvetica, sans-serif;"><?php echo $options["text"]; ?>&nbsp;&nbsp;
+		        <a class="apos_hello-link" href="#"><?php echo $options["button"]; ?></a> </span>
+		        <a class="close-notify">
+		            <img class="images/apos_hello-up-arrow" src="<?php echo plugins_url( '/apos_hello/img/apos_hello-up-arrow.png');  ?>" />
+		        </a>
+	        	</div>
+		<div class="apos_hello-stub" style="display: none;">
+		    <a class="show-notify">
+		        <img class="apos_hello-down-arrow" src="<?php echo plugins_url('/apos_hello/img/apos_hello-down-arrow.png'); ?>" />
+		    </a>
+		</div>
+
+		<?php
 	}
 
 	/**
@@ -111,10 +183,7 @@ class apos_hello {
 	 * Registers and enqueues admin-specific styles.
 	 */
 	public function register_admin_styles() {
-
-		// TODO change 'apos_hello' to the name of your plugin
 		wp_enqueue_style( 'apos_hello-admin-styles', plugins_url( 'apos_hello/css/admin.css' ) );
-
 	} // end register_admin_styles
 
 	/**
@@ -160,11 +229,11 @@ class apos_hello {
 	 *
 	 */
 	function action_method_name() {
-    	// TODO define your action method here
+		$options = get_option('apos_bar_options');
 		?>
 	<div class="apos_hello" style="display: none;">
-	    <span style="font-family: 'Arial, Helvetica, sans-serif;">PUT YOUR apos_hello MESSAGE OR CALL TO ACTION HERE &nbsp;&nbsp;
-	        <a class="apos_hello-link" href="#">BUTTON TEXT</a> </span>
+	    <span style="font-family: 'Arial, Helvetica, sans-serif;"><?php echo $options["text"]; ?>&nbsp;&nbsp;
+	        <a class="apos_hello-link" href="#"><?php echo $options["button"]; ?></a> </span>
 	        <a class="close-notify">
 	            <img class="images/apos_hello-up-arrow" src="<?php echo plugins_url( '/apos_hello/img/apos_hello-up-arrow.png');  ?>" /></a>
 	        </div>
